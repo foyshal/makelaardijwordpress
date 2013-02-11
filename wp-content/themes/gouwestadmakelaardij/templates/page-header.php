bf15411930b5fdd0db2b5a943911a95a3ae2474f
Lex <lex@gouwestadmm.nl>
2013-02-11 15:24:14 +0100

    Page header niet meer weergeven op pagina's

--- wp-content/themes/gouwestadmakelaardij/templates/page-header.php
+++ wp-content/themes/gouwestadmakelaardij/templates/page-header.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="row-fluid">
-		<?php if (is_front_page()) { ?>
+		<?php if (is_page()) { ?>
 		<!-- don't display title (leave this blank) -->
 		<?php } else { ?>
 		<div class="page-header">
