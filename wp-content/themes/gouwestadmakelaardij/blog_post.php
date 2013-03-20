d8965a4a1ce8438e37c2c2052aff0ee235772dc5
Jeroen Priem <jeroen@gouwestadmm.nl>
2013-03-20 14:12:30 +0100

    Social media toegevoegd

--- wp-content/themes/gouwestadmakelaardij/blog_post.php
+++ wp-content/themes/gouwestadmakelaardij/blog_post.php
@@ -4,6 +4,15 @@ Template Name: Blog Post Gouwestad Makelaardij
 */
 ?>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/nl_NL/all.js#xfbml=1&appId=113978908731538";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
 
@@ -23,7 +32,16 @@ Template Name: Blog Post Gouwestad Makelaardij
 				<i class="icon-bookmark-empty"></i> <?php the_category(' '); ?> </p>
 				<i class="icon-tags"></i> Tags:
 				<?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?>
-				<i class="icon-print"></i> <a href="javascript:window.print()" rel="nofollow">Printen</a>
+				<i class="icon-share-alt"></i> Deel dit bericht<br>
+				<!-- AddThis Button BEGIN -->
+	            <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+	            <a class="addthis_button_preferred_1"></a>
+	            <a class="addthis_button_preferred_2"></a>
+	            <a class="addthis_button_preferred_3"></a>
+	            </div><br>
+	           <!-- AddThis Button END -->
+	            <i class="icon-thumbs-up"></i> Like ons
+				<div class="fb-like" data-href="https://www.facebook.com/pages/Gouwestad-Makelaardij/176712525694328?fref=ts" data-send="false" data-width="100%" data-show-faces="true"></div>
 			</div>
 			<div class="span8 offset1">
 				<?php the_content(); ?>
